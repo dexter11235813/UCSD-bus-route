bda433445bcc5347724111973bf364aea2317079
dexter11235813 <abgairola@gmail.com>
2016-10-26 17:28:22 -0700

    deleted bash files for data scraping

--- chron.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-while true
-do
-    ~/UCSD-bus\ map/route.sh
-    echo "Getting Information"
-    sleep 5
-done
--- route.sh
+++
@@ -1,2 +0,0 @@
-curl -s https://ucsdbus.com/Route/2092/Vehicles >> ~/UCSD-bus\ map/downloads/Route2092.txt
-echo >> ~/UCSD-bus\ map/downloads/Route2092.txt
