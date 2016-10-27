5c1d5e8075cd2047b30488cd38960fd9d91a38b7
dexter11235813 <abgairola@gmail.com>
2016-10-26 17:18:15 -0700

    get_data added to scrape data from API

--- download_data.R
+++ download_data.R
@@ -2,6 +2,19 @@ library(rvest)
 
 route_id = c('2092','312','314','1263','1264','1114','1113','3442','3440','3159','1098','2399','1434','313','3849')
   
-  
-url = paste0('https://ucsdbus.com/Route/',route_id[1])
+get_data = function(route_id)
+{
+url = paste0('https://ucsdbus.com/Route/',route_id[1],"/Vehicles")
+i = 0
+while(T)
+{
 data = read_html(url) %>% html_text()
+print(paste0("getting infromation ",i))
+i = i + 1
+write(data,"Route2092.txt",append = T)
+Sys.sleep(5)
+}
+}
+get_data(2092)
+
+
